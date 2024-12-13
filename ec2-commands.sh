b752bc3d49afbf76ca6fc0a6397f886c9c137e08
Teddy Kekana <teddy.kekana@sovtech.com>
2024-12-13 14:08:22 +0200

    feat: create key pair for ec2

--- ec2-commands.sh
+++ ec2-commands.sh
@@ -23,6 +23,7 @@ tar xzf prometheus-3.0.1.linux-amd64.tar.gz
 sudo mv prometheus-3.0.1.linux-amd64 /opt
 sudo ln -s prometheus-3.0.1.linux-amd64 /opt/prometheus
 ls -l /opt/prometheus
+sudo chown -R /opt/prometheus
 rm -rf xzf prometheus-3.0.1.linux-amd64.tar.gz prometheus-3.0.1.linux-amd64
 sudo sh -c 'cat << EOF >> /opt/prometheus/prometheus.yml
               
