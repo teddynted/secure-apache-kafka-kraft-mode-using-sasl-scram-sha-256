b930d85bb2c74cdcea5eef7839102b8d601d86ca
Teddy Kekana <teddy.kekana@sovtech.com>
2024-12-23 14:59:39 +0200

    refactor: update ec2 instance

--- upload-key-pair.sh
+++ upload-key-pair.sh
@@ -9,4 +9,6 @@ if [ "$key" = "$Available_key" ]; then
     echo "Key is available.."
     #Available_key=`aws ec2 describe-key-pairs --key-name $key --output`
     #aws s3 cp $key.pem s3://$Bucket/$key.pem
+    aws ec2 describe-key-pairs --key-name $key --output text > $key.pem
+    cat $key.pem
 fi
