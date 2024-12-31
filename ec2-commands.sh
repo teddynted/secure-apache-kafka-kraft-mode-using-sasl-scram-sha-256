08e1a64937da0d1c59f010ac2205431033cae486
Teddy Kekana <teddy.kekana@sovtech.com>
2024-12-31 12:00:45 +0200

    refactor: setup ec2 security group

--- ec2-commands.sh
+++ ec2-commands.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 PUBLIC_IP_ADDRESS=$(ec2-metadata --public-ipv4 | cut -d " " -f 2);
+PRIVATE_IP_ADDRESS=$(ec2-metadata --local-ipv4 | cut -d " " -f 2);
+IP_ADDRESS=0.0.0.0
 echo "PUBLIC_IP_ADDRESS: '$PUBLIC_IP_ADDRESS'"
+echo "PRIVATE_IP_ADDRESS: '$PRIVATE_IP_ADDRESS'"
 
 KRAFT_ADVERTISED_LISTENERS=$(cat /opt/kafka/config/kraft/server.properties | grep -c "advertised.listeners=SASL_SSL://$PUBLIC_IP_ADDRESS:9092")
 echo 'KRAFT_ADVERTISED_LISTENERS '$KRAFT_ADVERTISED_LISTENERS''
@@ -11,8 +14,8 @@ sudo sed -i s/offsets.topic.replication.factor=1/offsets.topic.replication.facto
 sudo sed -i s/transaction.state.log.replication.factor=1/transaction.state.log.replication.factor=2/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/socket.receive.buffer.bytes=102400/socket.receive.buffer.bytes=1048576/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/socket.send.buffer.bytes=102400/socket.send.buffer.bytes=1048576/ /opt/kafka/config/kraft/server.properties
-sudo sed -i s/controller.quorum.voters=1@localhost:9093/controller.quorum.voters=1@$PUBLIC_IP_ADDRESS:9093/ /opt/kafka/config/kraft/server.properties
-sudo sed -i s/listeners=PLAINTEXT:\\/\\/:9092,CONTROLLER:\\/\\/:9093/listeners=SASL_SSL:\\/\\/$PUBLIC_IP_ADDRESS:9092,CONTROLLER:\\/\\/$PUBLIC_IP_ADDRESS:9093/ /opt/kafka/config/kraft/server.properties
+sudo sed -i s/controller.quorum.voters=1@localhost:9093/controller.quorum.voters=1@$PRIVATE_IP_ADDRESS:9093/ /opt/kafka/config/kraft/server.properties
+sudo sed -i s/listeners=PLAINTEXT:\\/\\/:9092,CONTROLLER:\\/\\/:9093/listeners=SASL_SSL:\\/\\/$IP_ADDRESS:9092,CONTROLLER:\\/\\/$IP_ADDRESS:9093/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/inter.broker.listener.name=PLAINTEXT/inter.broker.listener.name=SASL_SSL/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/advertised.listeners=PLAINTEXT:\\/\\/localhost:9092,CONTROLLER:\\/\\/localhost:9093/advertised.listeners=SASL_SSL:\\/\\/$PUBLIC_IP_ADDRESS:9092/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/listener.security.protocol.map=CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT,SSL:SSL,SASL_PLAINTEXT:SASL_PLAINTEXT,SASL_SSL:SASL_SSL/listener.security.protocol.map=CONTROLLER:SASL_SSL,SSL:SSL,SASL_PLAINTEXT:SASL_PLAINTEXT,SASL_SSL:SASL_SSL/ /opt/kafka/config/kraft/server.properties
