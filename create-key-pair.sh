43828425c7f46d7e959a3d44fb8f70a900a16f46
Teddy Kekana <teddy.kekana@sovtech.com>
2024-12-31 13:14:27 +0200

    refactor: setup ec2 security group

--- create-key-pair.sh
+++ create-key-pair.sh
@@ -9,6 +9,7 @@ if [ "$key" = "$Available_key" ]; then
     echo "Key is available."
 else
     echo "Key is not available, Creating a new key"
+    # Create a new key pai
     aws ec2 create-key-pair \
     --key-name $key \
     --key-type rsa \
@@ -16,9 +17,11 @@ else
     --query "KeyMaterial" \
     --region $Region \
     --output text > $key.pem
+    # Create an s3 bucket and upload the key pair to s3
     aws s3api create-bucket \
         --acl private \
         --bucket $Bucket \
         --region $Region
+    # Upload to s3
     aws s3 cp $key.pem s3://$Bucket/$key.pem
 fi
