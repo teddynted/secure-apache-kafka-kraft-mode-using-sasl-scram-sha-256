b37a74b3512bb567e4b1112aeb44067b754a038f
Teddy Kekana <teddy.kekana@sovtech.com>
2025-04-05 01:09:55 +0200

    refactor: change lambda name

--- ec2-commands.sh
+++ ec2-commands.sh
@@ -31,5 +31,12 @@ sudo /opt/kafka/bin/kafka-topics.sh --create --bootstrap-server $PUBLIC_IP_ADDRE
 # List all the existing topics
 sudo /opt/kafka/bin/kafka-topics.sh --bootstrap-server $PUBLIC_IP_ADDRESS:9092 --list --command-config /opt/kafka/config/kraft/admin.config
 
+# Consuming Message
+
+#sudo /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server $PUBLIC_IP_ADDRESS:9092 --topic testtopic --from-beginning --consumer.config /opt/kafka/config/kraft/admin.config
+
+# Produce Messsage
+
+#sudo /opt/kafka/bin/kafka-console-producer.sh --bootstrap-server $PUBLIC_IP_ADDRESS:9092 --topic testtopic --producer.config /opt/kafka/config/kraft/admin.config
 
 
