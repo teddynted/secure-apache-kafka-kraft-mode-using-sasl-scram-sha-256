8e3440f84f055befff560e31a409c7d37ea1d2e4
Teddy Kekana <teddy.kekana@sovtech.com>
2025-04-04 22:52:14 +0200

    refactor: change instance name

--- ec2-commands.sh
+++ ec2-commands.sh
@@ -26,7 +26,7 @@ sudo systemctl status kafka
 # sudo systemctl list-unit-files --type=service
 
 # Create a topic if doesn't exists
-sudo /opt/kafka/bin/kafka-topics.sh --create --bootstrap-server $PUBLIC_IP_ADDRESS:9092 --replication-factor 1 --partitions 3 --topic testtopic --if-not-exists --command-config /opt/kafka/config/kraft/admin.config
+# sudo /opt/kafka/bin/kafka-topics.sh --create --bootstrap-server $PUBLIC_IP_ADDRESS:9092 --replication-factor 1 --partitions 3 --topic testtopic --if-not-exists --command-config /opt/kafka/config/kraft/admin.config
 
 # List all the existing topics
 sudo /opt/kafka/bin/kafka-topics.sh --bootstrap-server $PUBLIC_IP_ADDRESS:9092 --list --command-config /opt/kafka/config/kraft/admin.config
