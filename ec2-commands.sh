780bb434aa10809635a45d6ed49ba858372f7e93
Teddy Kekana <teddy.kekana@sovtech.com>
2025-04-15 23:05:20 +0200

    fix: debug apache kafka kraft installation

--- ec2-commands.sh
+++ ec2-commands.sh
@@ -17,6 +17,15 @@ KRAFT_ADVERTISED_LISTENERS=$(cat /opt/kafka/config/kraft/server.properties | gre
 echo 'KRAFT_ADVERTISED_LISTENERS '$KRAFT_ADVERTISED_LISTENERS''
 if [[ $KRAFT_ADVERTISED_LISTENERS -eq 0 ]] 
 then
+sudo touch /opt/kafka/config/kraft/client.properties
+sudo cat <<EOF > /opt/kafka/config/kraft/client.properties
+bootstrap.servers=$PRIVATE_DNS_NAME:9092
+security.protocol=SASL_SSL
+ssl.truststore.location=/opt/kafka/config/kafka-ssl/truststore/kafka.truststore.jks
+ssl.truststore.password=$1
+sasl.mechanism=SCRAM-SHA-256
+sasl.jaas.config=org.apache.kafka.common.security.scram.ScramLoginModule required username=$2 password=$1;
+EOF
 sudo sed -i s/offsets.topic.replication.factor=1/offsets.topic.replication.factor=2/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/transaction.state.log.replication.factor=1/transaction.state.log.replication.factor=2/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/socket.receive.buffer.bytes=102400/socket.receive.buffer.bytes=1048576/ /opt/kafka/config/kraft/server.properties
@@ -24,9 +33,9 @@ sudo sed -i s/socket.send.buffer.bytes=102400/socket.send.buffer.bytes=1048576/ 
 sudo sed -i s/controller.quorum.voters=1@localhost:9093/controller.quorum.voters=1@$PRIVATE_DNS_NAME:9093/ /opt/kafka/config/kraft/server.properties
 #sudo sed -i s/listeners=PLAINTEXT:\\/\\/:9092,CONTROLLER:\\/\\/:9093/listeners=SASL_SSL:\\/\\/$PRIVATE_DNS_NAME\\/:9092,INTERNAL:\\/\\/$PRIVATE_IP_ADDRESS:9094,CONTROLLER:\\/\\/$PRIVATE_IP_ADDRESS\\/:9093/ /opt/kafka/config/kraft/server.properties
 # Note that the hostname here is optional, the absence of hostname represents binding to 0.0.0.0 i.e., all interfaces
-sudo sed -i s/listeners=PLAINTEXT:\\/\\/:9092,CONTROLLER:\\/\\/:9093/listeners=SASL_SSL:\\/\\/:9092,CONTROLLER:\\/\\/:9093,BROKER:\\/\\/:9094/ /opt/kafka/config/kraft/server.properties
-sudo sed -i s/inter.broker.listener.name=PLAINTEXT/inter.broker.listener.name=BROKER/ /opt/kafka/config/kraft/server.properties
-sudo sed -i s/advertised.listeners=PLAINTEXT:\\/\\/localhost:9092,CONTROLLER:\\/\\/localhost:9093/advertised.listeners=SASL_SSL:\\/\\/$PUBLIC_DNS_NAME:9092,BROKER:\\/\\/$PRIVATE_DNS_NAME:9094/ /opt/kafka/config/kraft/server.properties
+sudo sed -i s/listeners=PLAINTEXT:\\/\\/:9092,CONTROLLER:\\/\\/:9093/listeners=SASL_SSL:\\/\\/:9092,CONTROLLER:\\/\\/:9093/ /opt/kafka/config/kraft/server.properties
+sudo sed -i s/inter.broker.listener.name=PLAINTEXT/inter.broker.listener.name=SASL_SSL/ /opt/kafka/config/kraft/server.properties
+sudo sed -i s/advertised.listeners=PLAINTEXT:\\/\\/localhost:9092,CONTROLLER:\\/\\/localhost:9093/advertised.listeners=SASL_SSL:\\/\\/$PRIVATE_DNS_NAME:9092/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/listener.security.protocol.map=CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT,SSL:SSL,SASL_PLAINTEXT:SASL_PLAINTEXT,SASL_SSL:SASL_SSL/listener.security.protocol.map=CONTROLLER:SASL_SSL,BROKER:SASL_SSL,SASL_SSL:SASL_SSL/ /opt/kafka/config/kraft/server.properties
 #sudo echo CN=${PRIVATE_DNS_NAME} >> /etc/environment
 # sudo sh -c 'cat << EOF >> /opt/kafka/config/kraft/server.properties
