9f726ae23f6c50808c87ff3d66537f82605b5221
Teddy Kekana <teddy.kekana@sovtech.com>
2025-04-16 00:16:29 +0200

    fix: debug apache kafka kraft installation

--- ec2-commands.sh
+++ ec2-commands.sh
@@ -7,8 +7,6 @@ echo "EC2 PUBLIC_IP_ADDRESS: '$PUBLIC_IP_ADDRESS'"
 echo "EC2 PRIVATE_IP_ADDRESS: '$PRIVATE_IP_ADDRESS'"
 PRIVATE_DNS_NAME="ip-${PRIVATE_IP_ADDRESS//./-}.$3.compute.internal"
 PUBLIC_DNS_NAME="ec2-${PUBLIC_IP_ADDRESS//./-}.$3.compute.amazonaws.com"
-#PRIVATE_DNS_NAME=$(curl -s http://${PRIVATE_IP_ADDRESS}/latest/meta-data/hostname)
-#PUBLIC_DNS_NAME=$(curl -s http://${PUBLIC_IP_ADDRESS}/latest/meta-data/public-hostname)
 echo "EC2 PUBLIC_DNS_NAME: '$PUBLIC_DNS_NAME'"
 echo "EC2 PRIVATE_DNS_NAME: '$PRIVATE_DNS_NAME'"
 
@@ -22,8 +20,6 @@ sudo sed -i s/transaction.state.log.replication.factor=1/transaction.state.log.r
 sudo sed -i s/socket.receive.buffer.bytes=102400/socket.receive.buffer.bytes=1048576/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/socket.send.buffer.bytes=102400/socket.send.buffer.bytes=1048576/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/controller.quorum.voters=1@localhost:9093/controller.quorum.voters=1@$PRIVATE_DNS_NAME:9093/ /opt/kafka/config/kraft/server.properties
-#sudo sed -i s/listeners=PLAINTEXT:\\/\\/:9092,CONTROLLER:\\/\\/:9093/listeners=SASL_SSL:\\/\\/$PRIVATE_DNS_NAME\\/:9092,INTERNAL:\\/\\/$PRIVATE_IP_ADDRESS:9094,CONTROLLER:\\/\\/$PRIVATE_IP_ADDRESS\\/:9093/ /opt/kafka/config/kraft/server.properties
-# Note that the hostname here is optional, the absence of hostname represents binding to 0.0.0.0 i.e., all interfaces
 sudo sed -i s/listeners=PLAINTEXT:\\/\\/:9092,CONTROLLER:\\/\\/:9093/listeners=SASL_SSL:\\/\\/:9092,CONTROLLER:\\/\\/:9093/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/inter.broker.listener.name=PLAINTEXT/inter.broker.listener.name=SASL_SSL/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/advertised.listeners=PLAINTEXT:\\/\\/localhost:9092,CONTROLLER:\\/\\/localhost:9093/advertised.listeners=SASL_SSL:\\/\\/$PRIVATE_DNS_NAME:9092,CONTROLLER:\\/\\/$PRIVATE_DNS_NAME:9093/ /opt/kafka/config/kraft/server.properties
@@ -52,8 +48,12 @@ sudo sed -i s/listener.security.protocol.map=CONTROLLER:PLAINTEXT,PLAINTEXT:PLAI
 #   username='$2' \
 #   password='$1';
 # EOF'
+# sudo /opt/kafka/bin/kafka-metadata-quorum.sh --bootstrap-server ip-172-31-20-235.eu-west-1.compute.internal:9092 describe --status
+# sudo /opt/kafka/bin/kafka-metadata-quorum.sh --bootstrap-server ip-172-31-20-235.eu-west-1.compute.internal:9092 --command-config /opt/kafka/config/kraft/client.properties describe --status
+# sudo /opt/kafka/bin/kafka-topics.sh --create --bootstrap-server ip-172-31-20-235.eu-west-1.compute.internal:9092 --replication-factor 1 --partitions 3 --topic testtopic --if-not-exists --command-config /opt/kafka/config/kraft/client.properties
+# sudo /opt/kafka/bin/kafka-topics.sh --bootstrap-server ip-172-31-20-235.eu-west-1.compute.internal:9092 --list --command-config /opt/kafka/config/kraft/client.properties
 # sudo /opt/kafka/bin/kafka-acls.sh --bootstrap-server $PUBLIC_IP_ADDRESS:9092 --add --allow-principal "User:broker1" --operation ClusterAction --cluster
-# sudo /opt/kafka/bin/kafka-acls.sh --bootstrap-server $PUBLIC_IP_ADDRESS:9092 --list --cluster
+# sudo /opt/kafka/bin/kafka-acls.sh --bootstrap-server ip-172-31-20-235.eu-west-1.compute.internal:9092 --list --cluster
 sudo touch /opt/kafka/config/kraft/client.properties
 sudo tee /opt/kafka/config/kraft/client.properties > /dev/null <<EOF
 bootstrap.servers=$PRIVATE_DNS_NAME:9092
@@ -69,16 +69,16 @@ sudo systemctl start kafka
 sudo systemctl status kafka
 sudo /opt/kafka/bin/kafka-topics.sh --create --bootstrap-server $PRIVATE_DNS_NAME:9092 --replication-factor 1 --partitions 3 --topic testtopic --if-not-exists --command-config /opt/kafka/config/kraft/client.properties
 sudo /opt/kafka/bin/kafka-topics.sh --bootstrap-server $PRIVATE_DNS_NAME:9092 --list --command-config /opt/kafka/config/kraft/client.properties
-echo 'SASL_SCRAM_PASSWORD username'$2' password:'$1' region:'$3''
-#sudo /opt/kafka/bin/kafka-configs.sh --bootstrap-server $PUBLIC_IP_ADDRESS:9092 --alter --add-config 'SCRAM-SHA-256=[password='$1']' --entity-type users --entity-name admin
-# sudo /opt/kafka/bin/kafka-configs.sh --bootstrap-server $PRIVATE_IP_ADDRESS:9092 --alter --add-config 'SCRAM-SHA-256=[password='$1']' --entity-type users --entity-name broker
-# sudo /opt/kafka/bin/kafka-configs.sh --bootstrap-server $PRIVATE_IP_ADDRESS:9092 --alter --add-config 'SCRAM-SHA-256=[password='$1']' --entity-type users --entity-name controller
-# sudo /opt/kafka/bin/kafka-acls.sh --bootstrap-server $PRIVATE_IP_ADDRESS:9092 --add --allow-principal "User:admin" --operation ClusterAction --cluster
-# sudo /opt/kafka/bin/kafka-acls.sh --bootstrap-server $PRIVATE_IP_ADDRESS:9092 --add --allow-principal "User:broker" --operation ClusterAction --cluster
-# sudo /opt/kafka/bin/kafka-acls.sh --bootstrap-server $PRIVATE_IP_ADDRESS:9092 --add --allow-principal "User:controller" --operation ClusterAction --cluster
-# sudo systemctl restart kafka
-# sudo systemctl status kafka -l
-# sudo /opt/kafka/bin/kafka-metadata-quorum.sh --bootstrap-controller $PRIVATE_IP_ADDRESS:9093 describe --status
+sudo /opt/kafka/bin/kafka-metadata-quorum.sh --bootstrap-server $PRIVATE_DNS_NAME:9092 --command-config /opt/kafka/config/kraft/client.properties describe --status
+sudo /opt/kafka/bin/kafka-configs.sh --bootstrap-server ip-172-31-20-235.eu-west-1.compute.internal:9092 --command-config /opt/kafka/config/kraft/client.properties --alter --add-config 'SCRAM-SHA-256=[password='Passw0rd123']' --entity-type users --entity-name admin
+sudo /opt/kafka/bin/kafka-configs.sh --bootstrap-server $PRIVATE_IP_ADDRESS:9092 --command-config /opt/kafka/config/kraft/client.properties --alter --add-config 'SCRAM-SHA-256=[password='$1']' --entity-type users --entity-name broker
+sudo /opt/kafka/bin/kafka-configs.sh --bootstrap-server $PRIVATE_IP_ADDRESS:9092 --command-config /opt/kafka/config/kraft/client.properties --alter --add-config 'SCRAM-SHA-256=[password='$1']' --entity-type users --entity-name controller
+sudo /opt/kafka/bin/kafka-acls.sh --bootstrap-server $PRIVATE_IP_ADDRESS:9092 --command-config /opt/kafka/config/kraft/client.properties --add --allow-principal "User:admin" --operation ClusterAction --cluster
+sudo /opt/kafka/bin/kafka-acls.sh --bootstrap-server $PRIVATE_IP_ADDRESS:9092 --command-config /opt/kafka/config/kraft/client.properties --add --allow-principal "User:broker" --operation ClusterAction --cluster
+sudo /opt/kafka/bin/kafka-acls.sh --bootstrap-server $PRIVATE_IP_ADDRESS:9092 --command-config /opt/kafka/config/kraft/client.properties --add --allow-principal "User:controller" --operation ClusterAction --cluster
+sudo systemctl restart kafka
+sudo systemctl status kafka -l
+sudo /opt/kafka/bin/kafka-metadata-quorum.sh --bootstrap-controller $PRIVATE_IP_ADDRESS:9092 --command-config /opt/kafka/config/kraft/client.properties describe --status
 fi
 
 # cat /var/log/cloud-init-output.log
