d9014ad673c7075cfe042cc75b4b24a3f48443b0
Teddy Kekana <teddy.kekana@sovtech.com>
2025-04-15 16:53:40 +0200

    fix: debug apache kafka kraft installation

--- ec2-commands.sh
+++ ec2-commands.sh
@@ -17,7 +17,6 @@ KRAFT_ADVERTISED_LISTENERS=$(cat /opt/kafka/config/kraft/server.properties | gre
 echo 'KRAFT_ADVERTISED_LISTENERS '$KRAFT_ADVERTISED_LISTENERS''
 if [[ $KRAFT_ADVERTISED_LISTENERS -eq 0 ]] 
 then
-sudo echo CN=${PRIVATE_DNS_NAME} >> /etc/environment
 sudo sed -i s/offsets.topic.replication.factor=1/offsets.topic.replication.factor=2/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/transaction.state.log.replication.factor=1/transaction.state.log.replication.factor=2/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/socket.receive.buffer.bytes=102400/socket.receive.buffer.bytes=1048576/ /opt/kafka/config/kraft/server.properties
@@ -29,6 +28,7 @@ sudo sed -i s/listeners=PLAINTEXT:\\/\\/:9092,CONTROLLER:\\/\\/:9093/listeners=C
 sudo sed -i s/inter.broker.listener.name=PLAINTEXT/inter.broker.listener.name=BROKER/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/advertised.listeners=PLAINTEXT:\\/\\/localhost:9092,CONTROLLER:\\/\\/localhost:9093/advertised.listeners=CLIENT:\\/\\/$PUBLIC_DNS_NAME:9092,BROKER:\\/\\/$PRIVATE_IP_ADDRESS:9094/ /opt/kafka/config/kraft/server.properties
 sudo sed -i s/listener.security.protocol.map=CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT,SSL:SSL,SASL_PLAINTEXT:SASL_PLAINTEXT,SASL_SSL:SASL_SSL/listener.security.protocol.map=CLIENT:SASL_SSL,CONTROLLER:SASL_SSL,BROKER:SASL_SSL/ /opt/kafka/config/kraft/server.properties
+sudo echo CN=${PRIVATE_DNS_NAME} >> /etc/environment
 # sudo sh -c 'cat << EOF >> /opt/kafka/config/kraft/server.properties
 # client.bootstrap.servers=CONTROLLER://'$PRIVATE_IP_ADDRESS':9093
 # client.sasl.mechanism=SCRAM-SHA-256
