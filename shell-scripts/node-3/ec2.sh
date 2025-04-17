0ec1973aeefed58029fb88aa81cee81a7c5871f3
Teddy Kekana <teddy.kekana@sovtech.com>
2025-04-17 21:17:48 +0200

    feat: add multiple instance apache kafka setup

--- shell-scripts/node-3/ec2.sh
+++ shell-scripts/node-3/ec2.sh
@@ -2,6 +2,7 @@
 
 instance_id=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].InstanceId" --filters "Name=tag:Name,Values='Apache Kafka Kraft Instance 3'" "Name=instance-state-name,Values=running" --output text)
 availability_zone=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].Placement.AvailabilityZone" --filters "Name=tag:Name,Values='Apache Kafka Kraft Instance 3'" "Name=instance-state-name,Values=running" --output text)
+public_dns_name=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].PublicDnsName" --filters "Name=tag:Name,Values='Apache Kafka Kraft Instance 3'" "Name=instance-state-name,Values=running" --output text)
 private_dns_name=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].PrivateDnsName" --filters "Name=tag:Name,Values='Apache Kafka Kraft Instance 3'" "Name=instance-state-name,Values=running" --output text)
 public_ip_address=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].PublicIpAddress" --filters "Name=tag:Name,Values='Apache Kafka Kraft Instance 3'" "Name=instance-state-name,Values=running" --output text)
 private_dns_name_node_1=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].PrivateDnsName" --filters "Name=tag:Name,Values='Apache Kafka Kraft Instance 1'" "Name=instance-state-name,Values=running" --output text)
