dcb05a84c0fb7398bfb4293bfe4d97158db763e0
Teddy Kekana <teddy.kekana@sovtech.com>
2025-04-17 18:30:52 +0200

    feat: github actions go setup

--- shell-scripts/upload-key-pair.sh
+++ shell-scripts/upload-key-pair.sh
@@ -4,23 +4,18 @@ KeyPairName=$KEY_PAIR_NAME
 key=$KeyPairName-$Region
 Available_key=`aws ec2 describe-key-pairs --key-name $key | grep KeyName | awk -F\" '{print $4}'`
 
-if [ "$key" = "$Available_key" ]; then
-    echo "Key is available."
+# Create a new key pair
+aws ec2 create-key-pair \
+--key-name $key \
+--key-type rsa \
+--key-format pem \
+--query "KeyMaterial" \
+--region $Region \
+--output text > $key.pem
+if aws s3 ls "s3://$Bucket" 2>/dev/null; then
+    echo "Bucket exists"
+    # Upload to s3
+    aws s3 cp $key.pem s3://$Bucket/$key.pem
 else
-    echo "Key is not available, Creating a new key"
-    # Create a new key pair
-    aws ec2 create-key-pair \
-    --key-name $key \
-    --key-type rsa \
-    --key-format pem \
-    --query "KeyMaterial" \
-    --region $Region \
-    --output text > $key.pem
-    if aws s3 ls "s3://$Bucket" 2>/dev/null; then
-        echo "Bucket exists"
-        # Upload to s3
-        aws s3 cp $key.pem s3://$Bucket/$key.pem
-    else
-        echo "Bucket does not exist or you lack permissions"
-    fi
+    echo "Bucket does not exist or you lack permissions"
 fi
