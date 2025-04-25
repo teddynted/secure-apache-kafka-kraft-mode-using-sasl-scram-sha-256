aea47e924fa841fbafc1a64f0ec17e0a1cf7561a
Teddy Kekana <teddy.kekana@sovtech.com>
2025-04-25 17:18:04 +0200

    feat: add lambda with self-managed apache kakfa as an event source

--- shell-scripts/prothemeus/ec2-commands.sh
+++ shell-scripts/prothemeus/ec2-commands.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo "Configure Promotheus"
+KAFKA_BOOTSTRAP_SERVER=$1
+KAFKA_BOOTSTRAP_SERVER_TWO=$2
+KAFKA_BOOTSTRAP_SERVER_THREE=$3
+
+echo "KAFKA_BOOTSTRAP_SERVER $KAFKA_BOOTSTRAP_SERVER"
+echo "KAFKA_BOOTSTRAP_SERVER_TWO $KAFKA_BOOTSTRAP_SERVER_TWO"
+echo "KAFKA_BOOTSTRAP_SERVER_THREE $KAFKA_BOOTSTRAP_SERVER_THREE"
+echo "Setup Prometheus Configuration"
+
+sudo cat /etc/prometheus/prometheus.yml
+
+sudo systemctl daemon-reload
+sudo systemctl start prometheus
+sudo systemctl status prometheus
+
+#http://<prometheus-ip>:9090/graph
--- shell-scripts/prothemeus/ec2.sh
+++ shell-scripts/prothemeus/ec2.sh
@@ -4,6 +4,10 @@ instance_id=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].I
 public_dns_name=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].PublicDnsName" --filters "Name=tag:Name,Values='Prometheus Instance'" "Name=instance-state-name,Values=running" --output text)
 availability_zone=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].Placement.AvailabilityZone" --filters "Name=tag:Name,Values='Prometheus Instance'" "Name=instance-state-name,Values=running" --output text)
 
+KAFKA_BOOTSTRAP_SERVER=$KAFKA_BOOTSTRAP_SERVER
+KAFKA_BOOTSTRAP_SERVER_TWO=$KAFKA_BOOTSTRAP_SERVER_TWO
+KAFKA_BOOTSTRAP_SERVER_THREE=$KAFKA_BOOTSTRAP_SERVER_THREE
+
 # Generate RSA key pair
 tmpfile=$(mktemp /tmp/ssh.XXXXXX)
 ssh-keygen -C "eic temp key" -q -f $tmpfile -t rsa -b 2048 -N "" <<< y
@@ -18,4 +22,4 @@ aws ec2-instance-connect send-ssh-public-key \
   --availability-zone $availability_zone > /dev/null
 
 # SSH into ec2 instance with private key
-ssh -i $private_key -o "StrictHostKeyChecking no" ec2-user@$public_dns_name "bash -s" < ./shell-scripts/prothemeus/ec2-commands.sh 
+ssh -i $private_key -o "StrictHostKeyChecking no" ec2-user@$public_dns_name "bash -s" < ./shell-scripts/prothemeus/ec2-commands.sh $KAFKA_BOOTSTRAP_SERVER $KAFKA_BOOTSTRAP_SERVER_TWO $KAFKA_BOOTSTRAP_SERVER_THREE
