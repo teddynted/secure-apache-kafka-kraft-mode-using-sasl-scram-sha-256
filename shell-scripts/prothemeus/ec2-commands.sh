37f5a4aeeb93cb4ad05755819113874e0d51770c
Teddy Kekana <teddy.kekana@sovtech.com>
2025-04-26 04:14:25 +0200

    feat: add lambda with self-managed apache kakfa as an event source

--- shell-scripts/prothemeus/ec2-commands.sh
+++ shell-scripts/prothemeus/ec2-commands.sh
@@ -9,7 +9,7 @@ echo "KAFKA_BOOTSTRAP_SERVER_TWO $KAFKA_BOOTSTRAP_SERVER_TWO"
 echo "KAFKA_BOOTSTRAP_SERVER_THREE $KAFKA_BOOTSTRAP_SERVER_THREE"
 echo "Setup Prometheus Configuration"
 
-sudo cat /etc/prometheus/prometheus.yml
+#sudo cat /etc/prometheus/prometheus.yml
 
 # sudo sh -c 'cat << EOF >> /etc/prometheus/prometheus.yml
 
@@ -18,7 +18,7 @@ sudo cat /etc/prometheus/prometheus.yml
 #     - targets: ['$KAFKA_BOOTSTRAP_SERVER', '$KAFKA_BOOTSTRAP_SERVER_TWO', '$KAFKA_BOOTSTRAP_SERVER_THREE']
 # EOF'
 
-sudo cat /etc/prometheus/prometheus.yml
+#sudo cat /etc/prometheus/prometheus.yml
 
 sudo systemctl daemon-reload
 sudo systemctl start prometheus
