c2a7850641355d22079083ddf903b01f861ab1ec
Teddy Kekana <teddy.kekana@sovtech.com>
2025-04-26 04:43:15 +0200

    feat: add lambda with self-managed apache kakfa as an event source

--- shell-scripts/prothemeus/ec2-commands.sh
+++ shell-scripts/prothemeus/ec2-commands.sh
@@ -9,19 +9,19 @@ echo "KAFKA_BOOTSTRAP_SERVER_TWO $KAFKA_BOOTSTRAP_SERVER_TWO"
 echo "KAFKA_BOOTSTRAP_SERVER_THREE $KAFKA_BOOTSTRAP_SERVER_THREE"
 echo "Setup Prometheus Configuration"
 
-#sudo cat /etc/prometheus/prometheus.yml
+sudo cat /etc/prometheus/prometheus.yml
 
-# sudo sh -c 'cat << EOF >> /etc/prometheus/prometheus.yml
+sudo sh -c 'cat << EOF >> /etc/prometheus/prometheus.yml
 
-#   - job_name: 'kafka'
-#     static_configs:
-#     - targets: ['$KAFKA_BOOTSTRAP_SERVER', '$KAFKA_BOOTSTRAP_SERVER_TWO', '$KAFKA_BOOTSTRAP_SERVER_THREE']
-# EOF'
+  - job_name: 'kafka'
+    static_configs:
+    - targets: ['$KAFKA_BOOTSTRAP_SERVER', '$KAFKA_BOOTSTRAP_SERVER_TWO', '$KAFKA_BOOTSTRAP_SERVER_THREE']
+EOF'
 
-#sudo cat /etc/prometheus/prometheus.yml
+sudo cat /etc/prometheus/prometheus.yml
 
-# sudo systemctl daemon-reload
-# sudo systemctl start prometheus
-# sudo systemctl status prometheus
+sudo systemctl daemon-reload
+sudo systemctl start prometheus
+sudo systemctl status prometheus
 
 #http://<prometheus-ip>:9090/graph
