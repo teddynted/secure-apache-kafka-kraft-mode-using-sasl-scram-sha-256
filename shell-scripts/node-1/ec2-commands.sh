b0c53b0f37583ef6489935b328cb0b6017a76641
Teddy Kekana <teddy.kekana@sovtech.com>
2025-04-30 22:03:23 +0200

    fix: add an ssm parameter for nodes cluster id

--- shell-scripts/node-1/ec2-commands.sh
+++ shell-scripts/node-1/ec2-commands.sh
@@ -46,7 +46,7 @@ sudo systemctl enable kafka
 sudo systemctl start kafka
 sudo systemctl status kafka
 sudo sleep 10
-sudo /opt/kafka/bin/kafka-topics.sh --create --bootstrap-server $PRIVATE_DNS_NAME:9092 --replication-factor $10 --partitions $10 --topic $8 --if-not-exists --command-config /opt/kafka/config/kraft/client.properties
+sudo /opt/kafka/bin/kafka-topics.sh --create --bootstrap-server $PRIVATE_DNS_NAME:9092 --replication-factor 3 --partitions 3 --topic testtopic --if-not-exists --command-config /opt/kafka/config/kraft/client.properties
 # sudo sleep 10
 # sudo /opt/kafka/bin/kafka-topics.sh --bootstrap-server $PRIVATE_DNS_NAME:9092 --list --command-config /opt/kafka/config/kraft/client.properties
 # sudo sleep 10
