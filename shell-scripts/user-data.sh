0f92b8e13b5cd91f57a54013c18a72ea6b6fc2e1
Teddy Kekana <teddy.kekana@sovtech.com>
2025-04-30 11:34:04 +0200

    fix: memory issues

--- shell-scripts/user-data.sh
+++ shell-scripts/user-data.sh
@@ -137,9 +137,9 @@ EOF'
 #echo 'export KAFKA_HEAP_OPTS="-Xms1G -Xmx1G"' >> /etc/environment
 echo 'export KAFKA_HEAP_OPTS="-Xmx384m -Xms384m"' >> /etc/environment # for t3.micro instance
 echo 'export KAFKA_OPTS="-Djava.security.auth.login.config=/opt/kafka/config/kraft/jaas.conf"' >> /etc/environment
-# echo 'export KAFKA_JVM_PERFORMANCE_OPTS="-XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+ExplicitGCInvokesConcurrent"' >> /etc/environment
+echo 'export KAFKA_JVM_PERFORMANCE_OPTS="-XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+ExplicitGCInvokesConcurrent"' >> /etc/environment
 # Debugging errors
-# echo 'KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:/opt/kafka/config/kraft/log4j.properties"' >> /etc/environment
+echo 'KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:/opt/kafka/config/kraft/log4j.properties"' >> /etc/environment
 # Create Kafka service
 sudo cat <<EOF > /etc/systemd/system/kafka.service
 [Unit]
