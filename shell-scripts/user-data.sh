6cc7ef40bbd57f2151a7948efa7cfac9d6179b22
Teddy Kekana <teddy.kekana@sovtech.com>
2025-06-17 20:18:51 +0200

    feat: create a jump server to run multiple nodes at once

--- shell-scripts/user-data.sh
+++ shell-scripts/user-data.sh
@@ -192,6 +192,8 @@ log4j.logger.kafka.server.KafkaApis=DEBUG
 log4j.logger.kafka.network=DEBUG
 log4j.logger.org.apache.kafka.common.network.SaslChannelBuilder=DEBUG
 log4j.logger.org.apache.kafka.common.security.scram=DEBUG
+log4j.logger.org.apache.kafka=DEBUG
+log4j.logger.org.apache.kafka.common=DEBUG
 EOF
 
 sudo mkdir -p /var/lib/kafka/logs
