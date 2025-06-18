20ca8bd748d01396d2dfd20f58f40cc12e4d13f6
Teddy Kekana <teddy.kekana@sovtech.com>
2025-06-18 21:02:55 +0200

    feat: create a jump server to run multiple nodes at once

--- shell-scripts/user-data.sh
+++ shell-scripts/user-data.sh
@@ -252,9 +252,9 @@ log.segment.bytes=10485760
 EOF'
 
 # t2.medium
-#echo 'export KAFKA_HEAP_OPTS="-Xms1G -Xmx1G"' >> /etc/environment
+echo 'export KAFKA_HEAP_OPTS="-Xms1G -Xmx1G"' >> /etc/environment
 # t2.micro
-echo 'export KAFKA_HEAP_OPTS="-Xmx512M -Xms256M"' >> /etc/environment
+#echo 'export KAFKA_HEAP_OPTS="-Xmx512M -Xms256M"' >> /etc/environment
 echo 'export KAFKA_OPTS="-Djava.security.auth.login.config=/opt/kafka/config/kraft/jaas.conf"' >> /etc/environment
 echo 'export KAFKA_JVM_PERFORMANCE_OPTS="-XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+ExplicitGCInvokesConcurrent"' >> /etc/environment
 # Debugging errors
@@ -271,8 +271,8 @@ Type=simple
 User=ec2-user
 Restart=on-failure
 SyslogIdentifier=kafka
-Environment="KAFKA_HEAP_OPTS='-Xms1G -Xmx1G'"
-#Environment="KAFKA_HEAP_OPTS='-Xmx384m -Xms384m'"
+#Environment="KAFKA_HEAP_OPTS='-Xms1G -Xmx1G'"
+Environment="KAFKA_HEAP_OPTS='-Xmx384m -Xms384m'"
 Environment="CLUSTER_ID=$CLUSTER_ID"
 Environment="KAFKA_LOG4J_OPTS=-Dlog4j.configuration=file:/opt/kafka/config/kraft/log4j.properties"
 Environment="KAFKA_OPTS=-Djava.security.auth.login.config=/opt/kafka/config/kraft/jaas.conf"
